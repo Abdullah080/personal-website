f179feba7b354a974a81f7092ca55373a51737e0
Abdullah080 <abdullah.mohammed08@outlook.com>
2022-09-10 21:13:25 +0400

    updated main section + removed social icon from footer + add scroll icon + add thanks page

--- personal website/script.js
+++ personal website/script.js
@@ -1,3 +1,21 @@
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
 document.getElementById("currentYear").innerHTML = currentYear;
+
+let scrollUp = document.querySelector(".up");
+
+window.onscroll = function () {
+  console.log(this.scrollY);
+  if (this.scrollY >= 1250) {
+    scrollUp.classList.add("show");
+  } else {
+    scrollUp.classList.remove("show");
+  }
+};
+
+scrollUp.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
