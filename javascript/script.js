c8aa898bf716fda21564e3d23107965f2eea4278
Abdullah080 <abdullah.mohammed08@outlook.com>
2023-02-01 18:01:52 +0400

    Adding BEM stucture + enhancing html and css code + add downlad button feature in JavaScript to download pdf file + removing media queries because I want to improve them later

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+// Download pdf file when click the button
+document.getElementById("downloadButton").addEventListener("click", function () {
+  const link = document.createElement("a");
+  link.href = "../pdf/My Resume.pdf";
+  link.download = "My Resume.pdf";
+  link.click();
+});
+
+// change Year
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
+
+// Scroll to up
+let scrollUp = document.querySelector(".up");
+
+window.onscroll = function () {
+  console.log(this.scrollY);
+  if (this.scrollY >= 1250) {
+    scrollUp.classList.add("show");
+  } else {
+    scrollUp.classList.remove("show");
+  }
+};
+
+scrollUp.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
